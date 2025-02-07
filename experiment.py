ba1f87f03a8218ed6d7ac9263f7162529a5cf959
Amisha <amisha1324s@gmail.com>
2025-02-07 16:10:46 +0530

    refining code

--- experiment.py
+++ experiment.py
@@ -1,28 +1,23 @@
-# Copyright 2016, Yarin Gal, All rights reserved.
-# This code is based on the code by Jose Miguel Hernandez-Lobato used for his 
-# paper "Probabilistic Backpropagation for Scalable Learning of Bayesian Neural Networks".
-
-# This file contains code to train dropout networks on the UCI datasets using the following algorithm:
-# 1. Create 20 random splits of the training-test dataset.
-# 2. For each split:
-# 3.   Create a validation (val) set taking 20% of the training set.
-# 4.   Get best hyperparameters: dropout_rate and tau by training on (train-val) set and testing on val set.
-# 5.   Train a network on the entire training set with the best pair of hyperparameters.
-# 6.   Get the performance (MC RMSE and log-likelihood) on the test set.
-# 7. Report the averaged performance (Monte Carlo RMSE and log-likelihood) on all 20 splits.
-
+# import net
 import math
 import numpy as np
 import argparse
 import sys
+import os
+from net import net
 
-parser=argparse.ArgumentParser()
+# python -u "c:\Users\amish\Documents\open\DropoutUncertaintyExps\experiment.py" --dir bostonHousing
+parser = argparse.ArgumentParser()
 
-parser.add_argument('--dir', '-d', required=True, help='Name of the UCI Dataset directory. Eg: bostonHousing')
-parser.add_argument('--epochx','-e', default=500, type=int, help='Multiplier for the number of epochs for training.')
-parser.add_argument('--hidden', '-nh', default=2, type=int, help='Number of hidden layers for the neural net')
 
-args=parser.parse_args()
+parser.add_argument('--dir', '-d', required=True,
+                    help='Name of the UCI Dataset directory. Eg: bostonHousing')
+parser.add_argument('--epochx', '-e', default=2, type=int,
+                    help='Multiplier for the number of epochs for training.')
+parser.add_argument('--hidden', '-nh', default=2, type=int,
+                    help='Number of hidden layers for the neural net')
+
+args = parser.parse_args()
 
 data_directory = args.dir
 epochs_multiplier = args.epochx
@@ -30,188 +25,144 @@ num_hidden_layers = args.hidden
 
 sys.path.append('net/')
 
-import net
 
-# We delete previous results
+# Construct paths in a Windows-compatible way
+base_results_path = os.path.join("UCI_Datasets", data_directory, "results")
+base_data_path = os.path.join("UCI_Datasets", data_directory, "data")
 
-from subprocess import call
-
-
-_RESULTS_VALIDATION_LL = "./UCI_Datasets/" + data_directory + "/results/validation_ll_" + str(epochs_multiplier) + "_xepochs_" + str(num_hidden_layers) + "_hidden_layers.txt"
-_RESULTS_VALIDATION_RMSE = "./UCI_Datasets/" + data_directory + "/results/validation_rmse_" + str(epochs_multiplier) + "_xepochs_" + str(num_hidden_layers) + "_hidden_layers.txt"
-_RESULTS_VALIDATION_MC_RMSE = "./UCI_Datasets/" + data_directory + "/results/validation_MC_rmse_" + str(epochs_multiplier) + "_xepochs_" + str(num_hidden_layers) + "_hidden_layers.txt"
-
-_RESULTS_TEST_LL = "./UCI_Datasets/" + data_directory + "/results/test_ll_" + str(epochs_multiplier) + "_xepochs_" + str(num_hidden_layers) + "_hidden_layers.txt"
-_RESULTS_TEST_TAU = "./UCI_Datasets/" + data_directory + "/results/test_tau_" + str(epochs_multiplier) + "_xepochs_" + str(num_hidden_layers) + "_hidden_layers.txt"
-_RESULTS_TEST_RMSE = "./UCI_Datasets/" + data_directory + "/results/test_rmse_" + str(epochs_multiplier) + "_xepochs_" + str(num_hidden_layers) + "_hidden_layers.txt"
-_RESULTS_TEST_MC_RMSE = "./UCI_Datasets/" + data_directory + "/results/test_MC_rmse_" + str(epochs_multiplier) + "_xepochs_" + str(num_hidden_layers) + "_hidden_layers.txt"
-_RESULTS_TEST_LOG = "./UCI_Datasets/" + data_directory + "/results/log_" + str(epochs_multiplier) + "_xepochs_" + str(num_hidden_layers) + "_hidden_layers.txt"
-
-_DATA_DIRECTORY_PATH = "./UCI_Datasets/" + data_directory + "/data/"
-_DROPOUT_RATES_FILE = _DATA_DIRECTORY_PATH + "dropout_rates.txt"
-_TAU_VALUES_FILE = _DATA_DIRECTORY_PATH + "tau_values.txt"
-_DATA_FILE = _DATA_DIRECTORY_PATH + "data.txt"
-_HIDDEN_UNITS_FILE = _DATA_DIRECTORY_PATH + "n_hidden.txt"
-_EPOCHS_FILE = _DATA_DIRECTORY_PATH + "n_epochs.txt"
-_INDEX_FEATURES_FILE = _DATA_DIRECTORY_PATH + "index_features.txt"
-_INDEX_TARGET_FILE = _DATA_DIRECTORY_PATH + "index_target.txt"
-_N_SPLITS_FILE = _DATA_DIRECTORY_PATH + "n_splits.txt"
-
-def _get_index_train_test_path(split_num, train = True):
-    """
-       Method to generate the path containing the training/test split for the given
-       split number (generally from 1 to 20).
-       @param split_num      Split number for which the data has to be generated
-       @param train          Is true if the data is training data. Else false.
-       @return path          Path of the file containing the requried data
-    """
-    if train:
-        return _DATA_DIRECTORY_PATH + "index_train_" + str(split_num) + ".txt"
-    else:
-        return _DATA_DIRECTORY_PATH + "index_test_" + str(split_num) + ".txt" 
+result_files = {
+    "validation_ll": os.path.join(base_results_path, f"validation_ll_{epochs_multiplier}_xepochs_{num_hidden_layers}_hidden_layers.txt"),
+    "validation_rmse": os.path.join(base_results_path, f"validation_rmse_{epochs_multiplier}_xepochs_{num_hidden_layers}_hidden_layers.txt"),
+    "validation_MC_rmse": os.path.join(base_results_path, f"validation_MC_rmse_{epochs_multiplier}_xepochs_{num_hidden_layers}_hidden_layers.txt"),
+    "test_ll": os.path.join(base_results_path, f"test_ll_{epochs_multiplier}_xepochs_{num_hidden_layers}_hidden_layers.txt"),
+    "test_tau": os.path.join(base_results_path, f"test_tau_{epochs_multiplier}_xepochs_{num_hidden_layers}_hidden_layers.txt"),
+    "test_rmse": os.path.join(base_results_path, f"test_rmse_{epochs_multiplier}_xepochs_{num_hidden_layers}_hidden_layers.txt"),
+    "test_MC_rmse": os.path.join(base_results_path, f"test_MC_rmse_{epochs_multiplier}_xepochs_{num_hidden_layers}_hidden_layers.txt"),
+    "test_log": os.path.join(base_results_path, f"log_{epochs_multiplier}_xepochs_{num_hidden_layers}_hidden_layers.txt"),
+}
+# dictionary to map file paths with usable keys
+data_files = {
+    "dropout_rates": os.path.join(base_data_path, "dropout_rates.txt"),
+    "tau_values": os.path.join(base_data_path, "tau_values.txt"),
+    "data": os.path.join(base_data_path, "data.txt"),
+    "hidden_units": os.path.join(base_data_path, "n_hidden.txt"),
+    "epochs": os.path.join(base_data_path, "n_epochs.txt"),
+    "index_features": os.path.join(base_data_path, "index_features.txt"),
+    "index_target": os.path.join(base_data_path, "index_target.txt"),
+    "n_splits": os.path.join(base_data_path, "n_splits.txt"),
+}
 
 
-print ("Removing existing result files...")
-call(["rm", _RESULTS_VALIDATION_LL])
-call(["rm", _RESULTS_VALIDATION_RMSE])
-call(["rm", _RESULTS_VALIDATION_MC_RMSE])
-call(["rm", _RESULTS_TEST_LL])
-call(["rm", _RESULTS_TEST_TAU])
-call(["rm", _RESULTS_TEST_RMSE])
-call(["rm", _RESULTS_TEST_MC_RMSE])
-call(["rm", _RESULTS_TEST_LOG])
-print ("Result files removed.")
+def get_index_train_test_path(split_num, train=True):
+    """Generate path for train/test split"""
+    filename = f"index_train_{split_num}.txt" if train else f"index_test_{split_num}.txt"
+    return os.path.join(base_data_path, filename)  
+# saving the index for dataset splits
 
-# We fix the random seed
 
+print("Removing existing result files...")
+for file in result_files.values():
+    if os.path.exists(file):
+        os.remove(file)
+print("Result files removed.")
+
+# Fix random seed
 np.random.seed(1)
 
-print ("Loading data and other hyperparameters...")
-# We load the data
+print("Loading data and other hyperparameters...")
 
-data = np.loadtxt(_DATA_FILE)
+# Load data
+data = np.loadtxt(data_files["data"])
+n_hidden = int(np.loadtxt(data_files["hidden_units"]))
+n_epochs = int(np.loadtxt(data_files["epochs"]))
 
-# We load the number of hidden units
+index_features = np.loadtxt(data_files["index_features"])
+index_target = int(np.loadtxt(data_files["index_target"]))
 
-n_hidden = np.loadtxt(_HIDDEN_UNITS_FILE).tolist()
+X = data[:, [int(i) for i in index_features.tolist()]]
+# select all rows with given features
+y = data[:, index_target]
 
-# We load the number of training epocs
+n_splits = int(np.loadtxt(data_files["n_splits"]))
 
-n_epochs = np.loadtxt(_EPOCHS_FILE).tolist()
-
-# We load the indexes for the features and for the target
-
-index_features = np.loadtxt(_INDEX_FEATURES_FILE)
-index_target = np.loadtxt(_INDEX_TARGET_FILE)
-
-X = data[ : , [int(i) for i in index_features.tolist()] ]
-y = data[ : , int(index_target.tolist()) ]
-
-# We iterate over the training test splits
-
-n_splits = np.loadtxt(_N_SPLITS_FILE)
-print ("Done.")
+print("Done.")
 
 errors, MC_errors, lls = [], [], []
-for split in range(int(n_splits)):
+for split in range(n_splits):
+    print(f'Loading file: {get_index_train_test_path(split, train=True)}')
+    print(f'Loading file: {get_index_train_test_path(split, train=False)}')
 
-    # We load the indexes of the training and test sets
-    print ('Loading file: ' + _get_index_train_test_path(split, train=True))
-    print ('Loading file: ' + _get_index_train_test_path(split, train=False))
-    index_train = np.loadtxt(_get_index_train_test_path(split, train=True))
-    index_test = np.loadtxt(_get_index_train_test_path(split, train=False))
+    index_train = np.loadtxt(get_index_train_test_path(split, train=True))
+    index_test = np.loadtxt(get_index_train_test_path(split, train=False))
 
-    X_train = X[ [int(i) for i in index_train.tolist()] ]
-    y_train = y[ [int(i) for i in index_train.tolist()] ]
-    
-    X_test = X[ [int(i) for i in index_test.tolist()] ]
-    y_test = y[ [int(i) for i in index_test.tolist()] ]
+    X_train = X[[int(i) for i in index_train.tolist()]]
+    y_train = y[[int(i) for i in index_train.tolist()]]
+# X_train contains the selected feature values, and y_train contains target values for the given index
 
-    X_train_original = X_train
-    y_train_original = y_train
+    X_test = X[[int(i) for i in index_test.tolist()]]
+    y_test = y[[int(i) for i in index_test.tolist()]]
+
     num_training_examples = int(0.8 * X_train.shape[0])
     X_validation = X_train[num_training_examples:, :]
     y_validation = y_train[num_training_examples:]
-    X_train = X_train[0:num_training_examples, :]
-    y_train = y_train[0:num_training_examples]
-    
-    # Printing the size of the training, validation and test sets
-    print ('Number of training examples: ' + str(X_train.shape[0]))
-    print ('Number of validation examples: ' + str(X_validation.shape[0]))
-    print ('Number of test examples: ' + str(X_test.shape[0]))
-    print ('Number of train_original examples: ' + str(X_train_original.shape[0]))
+    X_train = X_train[:num_training_examples, :]
+    y_train = y_train[:num_training_examples]
+# training examples spits into training and validation set
+    print(f'Number of training examples: {X_train.shape[0]}')
+    print(f'Number of validation examples: {X_validation.shape[0]}')
+    print(f'Number of test examples: {X_test.shape[0]}')
 
-    # List of hyperparameters which we will try out using grid-search
-    dropout_rates = np.loadtxt(_DROPOUT_RATES_FILE).tolist()
-    tau_values = np.loadtxt(_TAU_VALUES_FILE).tolist()
+    dropout_rates = np.loadtxt(data_files["dropout_rates"]).tolist()
+    tau_values = np.loadtxt(data_files["tau_values"]).tolist()
 
-    # We perform grid-search to select the best hyperparameters based on the highest log-likelihood value
     best_network = None
     best_ll = -float('inf')
-    best_tau = 0
-    best_dropout = 0
+    best_tau, best_dropout = 0, 0
+
     for dropout_rate in dropout_rates:
         for tau in tau_values:
-            print ('Grid search step: Tau: ' + str(tau) + ' Dropout rate: ' + str(dropout_rate))
-            network = net.net(X_train, y_train, ([ int(n_hidden) ] * num_hidden_layers),
-                    normalize = True, n_epochs = int(n_epochs * epochs_multiplier), tau = tau,
-                    dropout = dropout_rate)
-
-            # We obtain the test RMSE and the test ll from the validation sets
+            print(f'Grid search step: Tau: {tau} Dropout rate: {dropout_rate}')
+            network = net.net(X_train, y_train, ([n_hidden] * num_hidden_layers),
+                              normalize=True, n_epochs=int(n_epochs * epochs_multiplier),
+                              tau=tau, dropout=dropout_rate)
 
             error, MC_error, ll = network.predict(X_validation, y_validation)
-            if (ll > best_ll):
+            if ll > best_ll:
                 best_ll = ll
                 best_network = network
-                best_tau = tau
-                best_dropout = dropout_rate
-                print ('Best log_likelihood changed to: ' + str(best_ll))
-                print ('Best tau changed to: ' + str(best_tau))
-                print ('Best dropout rate changed to: ' + str(best_dropout))
-            
-            # Storing validation results
-            with open(_RESULTS_VALIDATION_RMSE, "a") as myfile:
-                myfile.write('Dropout_Rate: ' + repr(dropout_rate) + ' Tau: ' + repr(tau) + ' :: ')
-                myfile.write(repr(error) + '\n')
+                best_tau, best_dropout = tau, dropout_rate
+                print(f'Best log_likelihood changed to: {best_ll}')
+                print(f'Best tau changed to: {best_tau}')
+                print(f'Best dropout rate changed to: {best_dropout}')
 
-            with open(_RESULTS_VALIDATION_MC_RMSE, "a") as myfile:
-                myfile.write('Dropout_Rate: ' + repr(dropout_rate) + ' Tau: ' + repr(tau) + ' :: ')
-                myfile.write(repr(MC_error) + '\n')
+            with open(result_files["validation_rmse"], "a") as myfile:
+                myfile.write(f'Dropout_Rate: {dropout_rate} Tau: {tau} :: {error}\n')
 
-            with open(_RESULTS_VALIDATION_LL, "a") as myfile:
-                myfile.write('Dropout_Rate: ' + repr(dropout_rate) + ' Tau: ' + repr(tau) + ' :: ')
-                myfile.write(repr(ll) + '\n')
+            with open(result_files["validation_MC_rmse"], "a") as myfile:
+                myfile.write(f'Dropout_Rate: {dropout_rate} Tau: { tau} :: {MC_error}\n')
 
-    # Storing test results
-    best_network = net.net(X_train_original, y_train_original, ([ int(n_hidden) ] * num_hidden_layers),
-                    normalize = True, n_epochs = int(n_epochs * epochs_multiplier), tau = best_tau,
-                    dropout = best_dropout)
+            with open(result_files["validation_ll"], "a") as myfile:
+                myfile.write(f'Dropout_Rate: { dropout_rate} Tau: {tau} :: {ll}\n')
+
+    best_network = net.net(X_train, y_train, ([n_hidden] * num_hidden_layers),
+                           normalize=True, n_epochs=int(n_epochs * epochs_multiplier),
+                           tau=best_tau, dropout=best_dropout)
+
     error, MC_error, ll = best_network.predict(X_test, y_test)
-    
-    with open(_RESULTS_TEST_RMSE, "a") as myfile:
-        myfile.write(repr(error) + '\n')
 
-    with open(_RESULTS_TEST_MC_RMSE, "a") as myfile:
-        myfile.write(repr(MC_error) + '\n')
+    with open(result_files["test_rmse"], "a") as myfile:
+        myfile.write(f'{error}\n')
 
-    with open(_RESULTS_TEST_LL, "a") as myfile:
-        myfile.write(repr(ll) + '\n')
+    with open(result_files["test_MC_rmse"], "a") as myfile:
+        myfile.write(f'{MC_error}\n')
 
-    with open(_RESULTS_TEST_TAU, "a") as myfile:
-        myfile.write(repr(best_network.tau) + '\n')
+    with open(result_files["test_ll"], "a") as myfile:
+        myfile.write(f'{ll}\n')
 
-    print ("Tests on split " + str(split) + " complete.")
-    errors += [error]
-    MC_errors += [MC_error]
-    lls += [ll]
+    with open(result_files["test_tau"], "a") as myfile:
+        myfile.write(f'{best_tau}\n')
 
-with open(_RESULTS_TEST_LOG, "a") as myfile:
-    myfile.write('errors %f +- %f (stddev) +- %f (std error), median %f 25p %f 75p %f \n' % (
-        np.mean(errors), np.std(errors), np.std(errors)/math.sqrt(n_splits),
-        np.percentile(errors, 50), np.percentile(errors, 25), np.percentile(errors, 75)))
-    myfile.write('MC errors %f +- %f (stddev) +- %f (std error), median %f 25p %f 75p %f \n' % (
-        np.mean(MC_errors), np.std(MC_errors), np.std(MC_errors)/math.sqrt(n_splits),
-        np.percentile(MC_errors, 50), np.percentile(MC_errors, 25), np.percentile(MC_errors, 75)))
-    myfile.write('lls %f +- %f (stddev) +- %f (std error), median %f 25p %f 75p %f \n' % (
-        np.mean(lls), np.std(lls), np.std(lls)/math.sqrt(n_splits), 
-        np.percentile(lls, 50), np.percentile(lls, 25), np.percentile(lls, 75)))
+    with open(result_files["test_log"], "a") as myfile:
+        myfile.write(f'Split {split}: Best tau: {best_tau}, Best dropout: { best_dropout}, LL: {best_ll}\n')
+
+print("Training completed!")
